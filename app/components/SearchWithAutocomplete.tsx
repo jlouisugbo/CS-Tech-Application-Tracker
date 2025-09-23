22cd026ebdd61698c08e52bb03f4fde0e9ad00aa
Joel Louis-Ugbo <jlouisugbo@gmail.com>
2025-09-23 19:24:19 -0400

    quick fixes to SearchWithAutocomplete

--- app/components/SearchWithAutocomplete.tsx
+++ app/components/SearchWithAutocomplete.tsx
@@ -130,7 +130,7 @@ export function SearchWithAutocomplete({
               {filteredSuggestions.map((suggestion, index) => (
                 <li
                   key={index}
-                  ref={el => suggestionRefs.current[index] = el}
+                  ref={(el) => {suggestionRefs.current[index] = el}}
                   onClick={() => handleSuggestionClick(suggestion)}
                   className={`px-4 py-2 cursor-pointer transition-colors ${
                     index === activeSuggestionIndex
